c973b76187ed22ffafb983d79c57591fcfb43c25
icaro2222 <icarodias2222@gmail.com>
2022-10-08 15:48:47 -0300

    subindo alterações

---
+++ app/src/main/java/com/example/logincadastro/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.logincadastro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_novo_cadastro = (Button) findViewById(R.id.btn_novo_cadastro);
+        Button btn_entrar = (Button) findViewById(R.id.btn_entrar);
+        EditText input_email = findViewById(R.id.input_email);
+        EditText input_senha = findViewById(R.id.input_senha);
+
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(input_email.getText().toString().isEmpty()){
+                    input_email.setError("Campo Vazio");
+                }else if(input_senha.getText().toString().isEmpty()){
+                    input_senha.setError("Campo Vazio");
+                }else{
+                    Intent intent = new Intent(MainActivity.this, inicio.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        btn_novo_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("Validando...");
+                progressDialog.show();
+
+                Intent intent = new Intent(MainActivity.this, Cadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
